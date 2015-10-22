13f824562fb07ca5c4bc0973a41a8f038a48d583
Giulio Lunati <giuliolunati@gmail.com>
2015-10-22 18:13:01 +0200

    Fix shell commands for old versions compatibility

--- android/Common/src/com/googlecode/android_scripting/interpreter/InterpreterConfiguration.java
+++ android/Common/src/com/googlecode/android_scripting/interpreter/InterpreterConfiguration.java
@@ -258,7 +258,7 @@ public class InterpreterConfiguration {
     // install r3
     path = new File(context.getFilesDir(), "usr/bin/r3");
     if (!path.isFile()) {
-      String[] cmds = {"/system/bin/sh","bin/kbox_shell","-c","kpkg i r3"};
+      String[] cmds = {"/system/bin/sh",context.getFilesDir()+"/bin/kbox_shell","-c","/bin/kpkg i r3"};
       ProcessBuilder pb = new ProcessBuilder(cmds);
       pb.directory(context.getFilesDir());
       try {
@@ -327,8 +327,8 @@ public class InterpreterConfiguration {
     mInterpreterSet.add(new KboxInterpreter(
       "Maintenance Shell",
       "/system/bin/sh",
-      "-l",
-      "-l,%s",
+      "",
+      "%s",
       ".sh",
       "/",
       kbox
